f3aeee6fa8990897e374dc6f703ce0d0170647a9
Navkaran Singh Sidhu <nsidhu10@academic.rrc.ca>
2025-01-13 09:58:50 -0600

    Completed LibraryItem Class and added Accessors

---
+++ library_item/library_item.py
@@ -0,0 +1,74 @@
+""""
+Description: A class to manage LibraryItem objects.
+"""
+__author__ = "Navkaran Singh Sidhu"
+__version__ = "1.0.0"
+
+from genre.genre import Genre
+
+
+
+class LibraryItem:
+    """
+    """
+    def __init__(self, title : str, 
+                 author : str ,
+                 genre : Genre ):
+        """
+        
+        """
+        if len(title.strip()) > 0:
+            self.__title = title
+        else:
+            raise ValueError("Title cannot be blank.")
+        
+        if len(author.strip()) > 0:
+            self.__author = author
+        else:
+            raise ValueError("Author cannot be blank.")
+
+        if isinstance(genre, Genre):
+            self.__genre = genre
+        else:
+            raise ValueError("Invalid Genre")
+
+    @property
+    def title(self) -> str:
+        """
+        Accessor for title atttibute.
+
+        Returns:
+            str: title of the LibraryItem.
+
+        """
+        return self.__title
+        
+    @property
+    def author(self) -> str:
+        """
+        Accessor for author atttibute.
+
+        Returns:
+            str: author of the LibraryItem.
+
+        """
+        return self.__author    
+
+    @property
+    def genre(self) -> Genre:
+        """
+        Accessor for genre attribute.
+
+        Returns:
+            Genre:  Enum value of genre.
+
+        """
+        return self.__genre
+
+
+
+
+
+
+
+        
